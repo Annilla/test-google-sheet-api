2b4849405bc39a423f2cb5676ce143e380764ca4
Anny Chang <annychang@kingstono365.onmicrosoft.com>
2022-05-12 11:17:28 +0800

    Commit made from VS Code Azure Static Web Apps

--- love-face-true/src/main.js
+++ love-face-true/src/main.js
@@ -13,5 +13,9 @@ createApp(App)
   .use(store)
   .use(vuetify)
   .use(VueGapi, {
+    apiKey: "AIzaSyBwZP_gOcqHZXBdb0EVTKAtKUx6ToM8vLc",
+    clientId: "514596526831-3skpbqsapmgs6q5s99m8d095svrhua7b.apps.googleusercontent.com",
+    discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+    scope: "https://www.googleapis.com/auth/spreadsheets",
   })
   .mount('#app')
